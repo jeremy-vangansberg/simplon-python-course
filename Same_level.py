dadcc64b2b8780474af6068f05664790a2e1bcbc
jeremy Vangansberg <jeremy.vangansberg@gmail.com>
2023-10-12 09:55:06 +0200

    correction des exos terminées, exemple lambda, recursif et arg des fonctions)

--- Same_level.py
+++ Same_level.py
@@ -3,3 +3,10 @@ def same_level_function(source):
         print("this is a function from a same level file")
     elif source == "Child":
         print("this is a function from a parent level file")
+
+
+def same_level_function2(source):
+    if source == "Same":
+        print("this is a function from a same level file")
+    elif source == "Child":
+        print("this is a function from a parent level file")
---
+++ exercice_package/parent_file.py
@@ -0,0 +1,13 @@
+def multiply(a:int,b:int) ->int:
+    return a*b
+
+# import sys
+# import os
+
+# # Get the parent directory
+# parent_dir = os.path.dirname(os.path.realpath(__file__))
+
+# print(parent_dir)
+
+# # Add the parent directory to sys.path
+# sys.path.append(parent_dir)
